f9a16b158f139cc272483f1b6afdd5ffbe4e9664
Akash Singh <akashsingh6280@gmail.com>
2021-09-02 18:00:29 +0530

    ADDED: Function call to add elements to binary tree

---
+++ src/com/bridgelabz/binarysearchtree/BinaryTreeMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.binarysearchtree;
+
+public class BinaryTreeMain {
+	public static void main(String[] args) {
+		
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+	}
+}
