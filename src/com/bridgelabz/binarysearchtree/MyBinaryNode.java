f72da06028bcfffa1624a6f162a9480fc592f8b6
Akash Singh <akashsingh6280@gmail.com>
2021-09-02 17:51:02 +0530

    ADDED: MyBinaryNode class to initialise key,left and right of a node

---
+++ src/com/bridgelabz/binarysearchtree/MyBinaryNode.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.binarysearchtree;
+
+public class MyBinaryNode<K extends Comparable<K>> {
+	K key;
+	MyBinaryNode<K> left;
+	MyBinaryNode<K> right;
+
+	public MyBinaryNode(K key){
+
+		this.key=key;
+		this.left=null;
+		this.right=null;
+		
+	}
+
+}
